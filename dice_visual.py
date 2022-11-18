ba96537cb7997bfd7ac584bc6d7b02a3dcea47ea
Mikhail Borisov <101577077+Borisov2M@users.noreply.github.com>
2022-11-18 17:51:35 +0300

    make some changes

--- dice_visual.py
+++ dice_visual.py
@@ -1,63 +1,38 @@
-import os
-import time
 import pygal
 
 from dice import Dice
-
-def input_data(checker, message):
-    # Ввод информации и количестве кубов, количестве граней куба, 
-    # количестве бросков
-    while True:
-        clear = lambda: os.system('cls')
-        clear()
-
-        try:
-            data = int(input(message))
-        except:
-            print('Введите число.')
-            time.sleep(3)
-        else:
-            if checker == True:
-                if data > 0:
-                    break
-                else:
-                    print('Введенное значение должно быть больше нуля')
-                    time.sleep(3)
-            else:
-                if data > 0 and data % 2 == 0:
-                    break
-                else:
-                    print('Введенное значение должно быть четным и больше нуля')
-                    time.sleep(3)
-    return data
 
 dice = []
 results = []
 dice_sides = []
 frequencies = []
 
-dice_numbers = input_data(True, 'Количество кубиков: ')
-
+dice_numbers = 5#Dice.input_data(True, 'Количество кубиков: ')
 if dice_numbers > 1:
-    approval = input('Количество граней кубика разные? (y/n)')
+    approval = 'n'#input('Количество граней кубика разные? (y/n)')
     if approval == 'y':
         for count in range(dice_numbers):
-            dice_sides += [input_data(False, 'Количество граней: ')]
+            dice_sides += [Dice.input_data(False, 'Количество граней: ')]
     else:
-        dice_sides += [input_data(False, 'Количество граней: ')]
+        side = 6 #Dice.input_data(False, 'Количество граней: ')
         for count in range(dice_numbers):
-            dice_sides += [dice_sides]
+            dice_sides += [side]
 else:
-    dice_sides += [input_data(False, 'Количество граней: ')]
+    dice_sides += [Dice.input_data(False, 'Количество граней: ')]
+r_n = 100 #Dice.input_data(True, 'Количество бросков: ')
 
-r_n = input_data(True, 'Количество бросков: ')
+
 
 print('Количестов кубиков: {}'.format(dice_numbers))
 print('Количестов граней: {}'.format(dice_sides))
 print('Количестов бросков: {}'.format(r_n))
 
-for d_c in range(1, dice_numbers+1):
-    dice += [Dice(dice_sides[d_c - 1])]
+
+
+for d_c in range(dice_numbers):
+    dice += [Dice(dice_sides[d_c])]
+
+
 
 if len(dice) > 1:
     for index in range(1, len(dice)):    
@@ -68,12 +43,13 @@ if len(dice) > 1:
 
     for index in range(1, len(dice)):
         max_result = dice[index-1].num_sides + dice[index].num_sides
+    print(max_result)
 
     for index in range(1, len(dice)):
-        for value in range(dice_numbers, max_result + 1):
+        for value in range(dice_numbers, max_result):
             frequency = results.count(value)
             frequencies += [frequency]
-    print(frequencies)
+    print(frequencies, 'Mnogo')
 else:   
     for roll_num in range(r_n):
         result = dice[0].roll()
@@ -83,19 +59,24 @@ else:
     for value in range(1, dice_sides[0] + 1):
         frequency = results.count(value)
         frequencies += [frequency]
-    print(frequencies)
+    print(frequencies, 'Malo')
+
+
 
 hist = pygal.Bar()
 hist.title = "Резульаты n бросков m куба/ов d{}".format(dice_sides)
 hist.title += "\n n = {}; m = {}".format(r_n, dice_numbers)
 if dice_numbers > 1:
-   for count in range(dice_numbers):
-        max_result = dice_numbers*dice_sides[count] + 1
-        hist.x_labels = list(range(dice_numbers, max_result))
+    if approval == 'y':
+        for count in range(dice_numbers):
+            maxresult = dice_numbers*dice_sides[count]
+            hist.x_labels = list(range(dice_numbers, maxresult))
+    else:
+        maxresult = dice_numbers*dice_sides[0] + 1
+        hist.x_labels = list(range(dice_numbers, maxresult))
 else:
     hist.x_labels = list(range(dice_numbers, (dice_numbers*dice_sides[0]) + 1))
 hist.x_title = 'Результы'
 hist.y_title = 'Частота результатов'
-
 hist.add('d{}'.format(dice_sides), frequencies)
 hist.render_to_file('dice_visual.svg')
