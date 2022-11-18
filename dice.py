e9a0dbbc4f6ac98b0bc7b4f4c970a53f5b486e2f
Mikhail Borisov <101577077+Borisov2M@users.noreply.github.com>
2022-11-18 17:53:48 +0300

    something done

--- dice.py
+++ dice.py
@@ -1,3 +1,6 @@
+import os
+import time
+
 from random import randint
 
 class Dice():
@@ -7,3 +10,30 @@ class Dice():
     
     def roll(self):
         return randint(1, self.num_sides)
+
+    def input_data(checker, message):
+    # Ввод информации и количестве кубов, количестве граней куба, 
+    # количестве бросков
+        while True:
+            clear = lambda: os.system('cls')
+            clear()
+
+            try:
+                data = int(input(message))
+            except:
+                print('Введите число.')
+                time.sleep(3)
+            else:
+                if checker == True:
+                    if data > 0:
+                        break
+                    else:
+                        print('Введенное значение должно быть больше нуля')
+                        time.sleep(3)
+                else:
+                    if data > 0 and data % 2 == 0:
+                        break
+                    else:
+                        print('Введенное значение должно быть четным и больше нуля')
+                        time.sleep(3)
+        return data
